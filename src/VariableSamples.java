ba5601b556edfd010f8a17b33bd33e694b0001ad
raffi hazizi <raffi hazizi@LAPTOP-AU8ROK64>
2023-03-03 23:33:19 +0700

    Raffi Hazizi

---
+++ src/VariableSamples.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author raffi hazizi
+ */
+public class VariableSamples {
+    public static void main(String[] args){
+        //deklarasi tipe data dengan nama variable
+        //result dan tipe data boolean
+        boolean result;
+        
+        //deklarasi tipe data dengan nama variable
+        //option dan tipe data char
+        char option;
+        option ='c';
+        
+        //deklarasi tipe data dengan nama variable
+        //grade,double tipe data dan telah di inisialisasi
+        //to 0.0
+        double grade = 0.0;
+    }
+}
---
+++ src/pbo_tk1b_2201082016/PBO_TK1B_2201082016.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo_tk1b_2201082016;
+
+/**
+ *
+ * @author raffi hazizi
+ */
+public class PBO_TK1B_2201082016 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
